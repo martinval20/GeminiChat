87132919b3134c2e0856700eef5d753e26ee2e44
MartinVal <martinval2000@hotmail.com>
2024-02-23 13:06:23 -0500

    last changes

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require('dotenv').config()
 const fs = require('fs')
 const genAI = new GoogleGenerativeAI(process.env.API_KEY)
 
-
-
 function fileToGenerativePart(path, mimeType){
     return{
         inlineData: {
@@ -33,4 +31,33 @@ async function runVisionComparation(){
     const text = response.text()
     console.log(text)
 }
-runVisionComparation()
+async function runChatText(){
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"})
+    const chat = model.startChat({
+        history: [
+            {
+                role: "user",
+                parts: "Hello. I have 2 dogs in my house."
+            },
+            {
+                role: "model",
+                parts: "Great to meet you. What would you like to know?"
+            }
+        ]
+    })
+    const msg = "How many paws are in my house?"
+    const result = await chat.sendMessage(msg)
+    const response = await result.response
+    const text = response.text()
+    console.log(text)
+}
+
+async function runEmedding(){
+    const model = genAI.getGenerativeModel({ model: "embedding-001"})
+    const text = "The quick brown fox jumped over the lazy dog"
+
+    const result = await model.embedContent(text)
+    const embedding = result.embedding
+    console.log(embedding)
+}
+runEmedding()
