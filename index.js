6f2c1c020c9efcdc920e56dfe24fc28688739794
MartinVal <martinval2000@hotmail.com>
2024-02-23 11:58:59 -0500

    comparation function

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const {GoogleGenerativeAI} = require('@google/generative-ai')
 require('dotenv').config()
-
+const fs = require('fs')
 const genAI = new GoogleGenerativeAI(process.env.API_KEY)
 
 
-async function run(){
+
+function fileToGenerativePart(path, mimeType){
+    return{
+        inlineData: {
+            data: Buffer.from(fs.readFileSync(path)).toString('base64'),
+            mimeType
+        }
+    }
+}
+async function runTextGenerator(){
     const model = genAI.getGenerativeModel({ model: "gemini-pro"})
     const prompt = "Write a story about a magic backpack."
     const result = await model.generateContent(prompt)
@@ -12,4 +21,16 @@ async function run(){
     const text = response.text()
     console.log(text)
 }
-run()
+async function runVisionComparation(){
+    const visionModel = genAI.getGenerativeModel({ model: "gemini-pro-vision"})
+    const prompt = "What's different between these Pictures?"
+    const imageParts = [
+        fileToGenerativePart("5.jpeg", "image/jpeg"),
+        fileToGenerativePart("10.jpeg", "image/jpeg"),
+    ]
+    const result = await visionModel.generateContent([prompt, ...imageParts])
+    const response = await result.response
+    const text = response.text()
+    console.log(text)
+}
+runVisionComparation()
