0870656a088ca6171de025e5febfb93b64d028c4
MartinVal <martinval2000@hotmail.com>
2024-02-23 11:23:29 -0500

    Generating a console response from a prompt

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ require('dotenv').config()
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY)
 
-const model = genAI.getGenerativeModel({ model: "gemini-pro"})
 
-console.log(model)
+async function run(){
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"})
+    const prompt = "Write a story about a magic backpack."
+    const result = await model.generateContent(prompt)
+    const response = await result.response
+    const text = response.text()
+    console.log(text)
+}
+run()
